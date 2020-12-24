31e1dbf4d362a31843688e301607e7122fe8dcee
koichi <koichi.tano@gmail.com>
2020-12-24 09:19:13 +0000

    Procfile

--- database/migrations/2020_12_22_092553_create_tasks_table.php
+++ database/migrations/2020_12_22_092553_create_tasks_table.php
@@ -13,7 +13,7 @@ class CreateTasksTable extends Migration
             $table->string('content');
             $table->string('status');
             $table->timestamps();
-            $table->bigInteger('user_id')->unsigned();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id') //外部キー制約
                   ->references('id')->on('users') //ｕｓｅｒｓテーブルのidを参照する
                   ->onDelete('cascade');  //ユーザーが削除されたら紐付くpostsも削除
@@ -23,6 +23,5 @@ class CreateTasksTable extends Migration
     public function down()
     {
         Schema::dropIfExists('tasks');
-            $table->dropColomn("user_id");
     }
 }
